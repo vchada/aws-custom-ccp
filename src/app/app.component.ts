c1554ed95656882faf188b0f64e877b3f9685cdd
Sourabh Agrawat <majorproj15@gmail.com>
2023-07-09 23:15:20 +0530

    made changes for user type

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -44,19 +44,6 @@ export class AppComponent implements OnInit {
       },
     });
 
-    // const contact = connect.core.getActiveContact();
-
-    // // Check if there is an active contact
-    // if (contact) {
-    //   const attributes = {
-    //     attribute1: 'value1',
-    //     attribute2: 'value2',
-    //     // Add more attributes as needed
-    //   };
-
-    //   // Set the contact attributes
-    //   contact.getAgent().updateContactAttributes(attributes);
-    // }
 
     // Listen for the 'contactIncoming' event
     connect.contact((contact: any) => {
@@ -105,52 +92,42 @@ export class AppComponent implements OnInit {
       });
     });
 
-    // connect.updateContactAttributes(params, function(err, data) {
-    //   if (err) console.log(err, err.stack); // an error occurred
-    //   else     console.log(data);           // successful response
-    // });
- 
-    // connect.agent.isUserAgent((value: any) => {
-    //   debugger;
-    // })
-
-    // Check if the connected user is an agent
-
 
     connect.agent((agent: any) => {
       // gather information about the agent
       this.agent = agent;
-      if (connect.core.getAgent().getType() === connect.AgentType.AGENT) {
-        // Logic for agents
-        console.log('Connected user is an agent');
-      } else if (connect.core.getAgent().getType() === connect.AgentType.SUPERVISOR) {
-        // Logic for supervisors
-        console.log('Connected user is a supervisor');
-      } else {
-        // Logic for other roles or unidentified users
-        console.log('Connected user role unknown');
-      }
+      // if (connect.core.getAgent().getType() === connect.AgentType.AGENT) {
+      //   // Logic for agents
+      //   console.log('Connected user is an agent');
+      // } else if (connect.core.getAgent().getType() === connect.AgentType.SUPERVISOR) {
+      //   // Logic for supervisors
+      //   console.log('Connected user is a supervisor');
+      // } else {
+      //   // Logic for other roles or unidentified users
+      //   console.log('Connected user role unknown');
+      // }
 
-      const agentSnapshot = agent.getRoutingProfile();
-      if (agentSnapshot.defaultOutboundQueue.queueId) {
-        // The agent has a supervisor routing profile assigned
-        console.log('Supervisor');
-        this.isUserAgent = false;
-      } else {
-        // The agent does not have a supervisor routing profile assigned
-        console.log('Agent');
-        this.isUserAgent = true;
-      }
+      const permission = agent.getPermissions();
+      if (permission) {
 
-      agent.onRoutingProfile((routingProfile: any) => {
-        if (routingProfile.defaultOutboundQueue.queueId) {
-          console.log('Role changed to Supervisor');
+        if(permission.length > 2) {
+          console.log('Supervisor');
           this.isUserAgent = false;
         } else {
-          console.log('Role changed to Agent');
+          console.log('Agent');
           this.isUserAgent = true;
         }
-      });
+      }
+
+      // agent.onRoutingProfile((routingProfile: any) => {
+      //   if (routingProfile.defaultOutboundQueue.queueId) {
+      //     console.log('Role changed to Supervisor');
+      //     this.isUserAgent = false;
+      //   } else {
+      //     console.log('Role changed to Agent');
+      //     this.isUserAgent = true;
+      //   }
+      // });
     });
 
     this.open();
